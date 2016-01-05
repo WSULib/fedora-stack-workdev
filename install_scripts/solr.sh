15c61bfe2bd4b04f55f6996e6a89a6037db80ae6
Cole Hudson <cole.hudson@wayne.edu>
2016-01-05 11:26:26 -0500

    added sleep to avoid potential race condition with solr/tomcat startup

--- install_scripts/solr.sh
+++ install_scripts/solr.sh
@@ -25,6 +25,13 @@ tar -xzf solr-"$SOLR_VERSION".tgz
 cp -v /tmp/solr-"$SOLR_VERSION"/dist/solr-"$SOLR_VERSION".war /var/lib/tomcat7/webapps/solr4.war
 service tomcat7 restart
 
+# Waiting for Solr war file to initialize and create the solr4 directory
+while [ ! -d /var/lib/tomcat7/webapps/solr4/WEB-INF/ ]
+do
+ echo "waiting for Solr to deploy..."
+ sleep 2
+done
+
 chown -hR tomcat7:tomcat7 /usr/share/tomcat7/lib
 
 cp $SHARED_DIR/downloads/solr/$SOLR_CATALINA_CONFIG /etc/tomcat7/Catalina/localhost/solr4.xml
