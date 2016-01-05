fdd3a384a9df9fdb27642ff5e2cc0efb8357883b
Graham Hukill <ghukill@gmail.com>
2016-01-05 10:28:14 -0500

    oaiprovider now using table creation SQL

--- install_scripts/oaiprovider.sh
+++ install_scripts/oaiprovider.sh
@@ -15,7 +15,13 @@ else
 fi
 #################################################################
 
+# create MySQL database, users, tables, then populate ('proai' hardcoded below as db name)
+echo "creating MySQL database, users, and tables"
+mysql --user=root --password=$SQL_PASSWORD < $SHARED_DIR/downloads/oaiprovider/oaiprovider_mysql_db_create.sql
+mysql --user=root --password=$SQL_PASSWORD proai < $SHARED_DIR/config/oaiprovider/oaiprovider_tables_create.sql
+
 # download in download dir from here: http://www.fedora-commons.org/software/repository
+# documentation: https://wiki.duraspace.org/display/FCSVCS/OAI+Provider+Configuration+Reference
 
 # unzip to tmp, move to Tomcat
 echo "unzipping and moving war file"
@@ -27,9 +33,10 @@ service tomcat7 restart
 
 # move config file to Tomcat
 echo "copying config template and replacing values"
-cp $SHARED_DIR/downloads/oaiprovider/proai.properties /var/lib/tomcat7/webapps/oaiprovider/WEB-INF/classes
+cp $SHARED_DIR/downloads/oaiprovider/proai.properties /var/lib/tomcat7/webapps/oaiprovider/WEB-INF/classes/
 sed -i "s/FEDORA_ADMIN_USERNAME/$FEDORA_ADMIN_USERNAME/g" /var/lib/tomcat7/webapps/oaiprovider/WEB-INF/classes/proai.properties
 sed -i "s/FEDORA_ADMIN_PASSWORD/$FEDORA_ADMIN_PASSWORD/g" /var/lib/tomcat7/webapps/oaiprovider/WEB-INF/classes/proai.properties
+sed -i "s/proai.db.url = jdbc:postgresql:\/\/localhost\/proai/proai.db.url = jdbc:mysql:\/\/localhost\/proai?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true\/g" /var/lib/tomcat7/webapps/oaiprovider/WEB-INF/classes/proai.properties
 
 # restart Tomcat
 service tomcat7 restart
