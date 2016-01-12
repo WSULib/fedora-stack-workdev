344b4b7278b1f4f159d63337286ac9c221956da7
Cole Hudson <cole.hudson@wayne.edu>
2016-01-12 12:27:26 -0500

    bag ingest script

--- install_scripts/cleanup.sh
+++ install_scripts/cleanup.sh
@@ -17,3 +17,7 @@ fi
 
 # copy apache / info file
 cp $SHARED_DIR/config/cleanup/index.php /var/www/wsuls/
+
+# ingest test bags
+cp $SHARED_DIR/downloads/ouroboros/ingest_bags.py /opt/ouroboros/
+sudo python /opt/ouroboros/ingest_bags.py
