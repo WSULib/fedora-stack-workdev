674e730221b4d48834ca050b086f08b00fb1d337
Graham Hukill <ghukill@gmail.com>
2016-03-29 13:26:15 -0400

    more updates to fed prov

--- install_scripts/fedora.sh
+++ install_scripts/fedora.sh
@@ -25,13 +25,19 @@ FEDORA_3_6="http://sourceforge.net/projects/fedora-commons/files/fedora/3.6.2/fc
 # Create MySQL DB
 mysql --user=root --password=$SQL_PASSWORD < $SHARED_DIR/downloads/fedora/fedora_mysql_db_create.sql
 
+# Create MySQL DB
+cp $SHARED_DIR/downloads/fedora/fedora_mysql_db_create.sql /tmp/fedora_mysql_db_create.sql
+sed -i "s/FEDORA_MYSQL_USERNAME/$FEDORA_MYSQL_USERNAME/g" /tmp/fedora_mysql_db_create.sql
+sed -i "s/FEDORA_MYSQL_PASSWORD/$FEDORA_MYSQL_PASSWORD/g" /tmp/fedora_mysql_db_create.sql
+mysql --user=root --password=$SQL_PASSWORD < /tmp/fedora_mysql_db_create.sql
+
 # Installation (copy and sed in the install.properties)
 cp $SHARED_DIR/downloads/fedora/install.properties /tmp/install.properties
 sed -i "s/FEDORA_ADMIN_USERNAME/$FEDORA_ADMIN_USERNAME/g" /tmp/install.properties
 sed -i "s/FEDORA_ADMIN_PASSWORD/$FEDORA_ADMIN_PASSWORD/g" /tmp/install.properties
 sed -i "s/FEDORA_MYSQL_USERNAME/$FEDORA_MYSQL_USERNAME/g" /tmp/install.properties
 sed -i "s/FEDORA_MYSQL_PASSWORD/$FEDORA_MYSQL_PASSWORD/g" /tmp/install.properties
-java -jar $SHARED_DIR/downloads/fedora/fcrepo-installer-3.8.1.jar $SHARED_DIR/downloads/fedora/install.properties
+java -jar $SHARED_DIR/downloads/fedora/fcrepo-installer-3.8.1.jar /tmp/install.properties
 
 # copy custom fedora.fcfg and replace values
 cp /opt/fedora/server/config/fedora.fcfg /opt/fedora/server/config/fedora.fcfg.BACKUP
